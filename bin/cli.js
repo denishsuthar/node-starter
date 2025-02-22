90100b7d0019aabd0745259182387c74b38c16fb
denish suthar <denish.v2web@gmail.com>
2025-02-22 20:25:11 +0530

    cli added

---
+++ bin/cli.js
@@ -0,0 +1,29 @@
+#!/usr/bin/env node
+import { execSync } from "child_process"
+
+const runCommand = command => {
+    try {
+        execSync(`${command}`, {stdio: 'inherit'})
+    } catch (error) {
+        console.error(`Failed to excute`, error)
+        return false
+    }
+    return true
+}
+
+const repoName = process.argv[2];
+const gitCheckoutCommand = `git clone --depth 1 https://github.com/denishsuthar/node-starter.git ${repoName}`
+const installDepsCommand = `cd ${repoName} && npm install`;
+
+console.log("Installing.......");
+
+const checkout = runCommand(gitCheckoutCommand)
+if(!checkout) process.exit(-1)
+
+console.log(`Installing dependencies for denish starter template`);
+
+const installDeps = runCommand(installDepsCommand)
+if(!installDeps) process.exit(-1)
+
+console.log(`Done....Start Backend using below command`);
+console.log(`cd ${repoName} && npm start`);
